0c0f5c3d362f15d320c169f15dc79fddd3b3dd1c
norderan <queider@proton.me>
2025-06-06 20:37:01 +0300

    Add dist

--- src/mapService.js
+++ src/mapService.js
@@ -1,19 +1,18 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-
-let map = L.map('map', {
-  zoomControl: false // disable default position
+const map = L.map('map', {
+  zoomControl: false, // disable default position
 }).setView([51.505, -0.09], 13); // london coordinates
 
 // add zoom control manually in a new position
 L.control.zoom({
-  position: 'bottomright' // or 'topright', 'bottomleft', 'topleft'
+  position: 'bottomright', // or 'topright', 'bottomleft', 'topleft'
 }).addTo(map);
 
 let currentTileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '&copy; OpenStreetMap contributors'
+  attribution: '&copy; OpenStreetMap contributors',
 }).addTo(map);
 
 function switchTileLayer() {
@@ -24,7 +23,7 @@ function switchTileLayer() {
     // Switch to normal
     currentTileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-      attribution: '&copy; OpenStreetMap contributors'
+      attribution: '&copy; OpenStreetMap contributors',
     }).addTo(map);
   } else {
     // Switch to dark
@@ -32,15 +31,13 @@ function switchTileLayer() {
       minZoom: 0,
       maxZoom: 20,
       attribution: '&copy; Stamen Design',
-      ext: 'png'
+      ext: 'png',
     }).addTo(map);
   }
-  }
-
-
-function setMapCoordinates(coordinates) {
-    map.setView([coordinates.lat, coordinates.lon], 10);
 }
 
+function setMapCoordinates(coordinates) {
+  map.setView([coordinates.lat, coordinates.lon], 10);
+}
 
 export { setMapCoordinates, map, switchTileLayer };
